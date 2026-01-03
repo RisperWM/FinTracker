6f256ecb9427e8bc9fc99f6d28146b4f1df2694c
RisperWM <risperwanjat@gmail.com>
2026-01-03 16:04:28 +0300

    fix:

--- Backend/src/firebaseAdmin.ts
+++ Backend/src/firebaseAdmin.ts
@@ -1,10 +1,21 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const serviceAccount = {
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    // We trim and ensure the replacement is globally applied
+    private_key: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY
+        : undefined,
+};
 
 if (!admin.apps.length) {
     admin.initializeApp({
+        // @ts-ignore - this prevents TS from complaining about the object type
         credential: admin.credential.cert(serviceAccount),
     });
 }
-
-module.exports = admin;
+module.exports= admin;
